b5a4d1244c68e7fc2a9e813cffca38f2414faa89
Ashley Cao <ashleycao12@gmail.com>
2023-06-18 16:21:03 +1200

    create home page

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim a officiis minus nostrum dolorum placeat fuga. Iure ex rerum odit dignissimos doloremque quis. Sit quo dolores voluptatibus rem illo cupiditate.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>

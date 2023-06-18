28c0f06ee9a61a30b0970c5e4b7e712eb0774b83
Ashley Cao <ashleycao12@gmail.com>
2023-06-18 22:51:51 +1200

    placeholder index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <template>
-  <div>
-    <h2>Home</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim a officiis minus nostrum dolorum placeat fuga. Iure ex rerum odit dignissimos doloremque quis. Sit quo dolores voluptatibus rem illo cupiditate.</p>
+  <div class="bg-slate-600">
+    <h2 class=" text-blue-500">Home</h2>
+    <p class="bg-blue-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim a officiis minus nostrum dolorum
+      placeat fuga. Iure
+      ex rerum odit dignissimos doloremque quis. Sit quo dolores voluptatibus rem illo cupiditate.</p>
   </div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

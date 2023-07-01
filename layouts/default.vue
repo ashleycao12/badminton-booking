dcc2ea6ff70d854a2aa2daa8fd1673c3458295d6
Ashley Cao <ashleycao12@gmail.com>
2023-07-01 15:16:37 +1200

    add some stuff

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/account">My account</NuxtLink>
+      </nav>
+    </header>
+  </div>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-slate-600">
-    <h2 class=" text-blue-500">Home</h2>
-    <p class="bg-blue-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim a officiis minus nostrum dolorum
+  <div>
+    <h2 class=" text-blue-500">Home Page</h2>
+    <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim a officiis minus nostrum dolorum
       placeat fuga. Iure
       ex rerum odit dignissimos doloremque quis. Sit quo dolores voluptatibus rem illo cupiditate.</p>
   </div>

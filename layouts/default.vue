a87c254211ec4fc4d8e9dfb15ab7bd546901aae9
Ashley Cao <ashleycao12@gmail.com>
2023-07-16 00:52:34 +1200

    language dynamic navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,8 @@
     <header>
       <nav class="flex items-center justify-center">
         <img src="logo.jpg" alt="">
-        <NuxtLink to="/">Home</NuxtLink>
-        <NuxtLink to="/account">My account</NuxtLink>
+        <NuxtLink to="/">{{ navContent.home }}</NuxtLink>
+        <NuxtLink to="/account">{{ navContent.account }}</NuxtLink>
       </nav>
     </header>
   </div>
@@ -13,8 +13,12 @@
   </div>
 </template>
 
-<script setup>
-
+<script setup lang="ts">
+import nav from '../content/nav.json'
+const route = useRoute()
+const lang: string = route.params.lang == '' ? 'eng' : route.params.lang
+const navContent: string = nav[lang]
+console.log(lang);
 </script>
 
 <style scoped></style>

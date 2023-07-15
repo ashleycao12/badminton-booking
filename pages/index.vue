d27b40e0bce3df99549d4306d44ae4f822277773
Ashley Cao <ashleycao12@gmail.com>
2023-07-15 23:23:57 +1200

    add content module

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,11 @@
     <h2 class=" text-blue-500">Home Page</h2>
     <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim a officiis minus nostrum dolorum
       placeat fuga. Iure
-      ex rerum odit dignissimos doloremque quis. Sit quo dolores voluptatibus rem illo cupiditate.</p>
+      ex rerum odit dignissimos doloremque quis. Sit quo dolores voluptatibus rem illo cupiditate.
+    </p>
+    <p>
+      <ContentDoc />
+    </p>
   </div>
 </template>
 

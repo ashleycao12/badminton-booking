d098747469f17bca4491963aa8b3fc181499fa1f
Ashley Cao <ashleycao12@gmail.com>
2023-08-13 18:27:57 +1200

    add login page

---
+++ pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bg-slate-200">
+    <form @submit.prevent="handleSubmit" class=" mt-24 flex flex-col items-center gap-5 p-5">
+      <div >
+        <label for="phone-number">{{$t("phone-number")}}: </label>
+        <input type="text" id="phone-number" v-model="phoneNumber" required class=" border border-gray-300">
+      </div>
+      <button class="submit bg-sky-600 text-white py-2 px-4 rounded-md" >{{ $t('sign-in') }}</button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+  // Disable default layout
+  definePageMeta({
+    layout: false,
+  });
+
+  const phoneNumber = ref('')
+
+  // stop letter to be typed into phone number field
+  watch(phoneNumber, () => {
+    const isNumber = /^\d+$/.test(phoneNumber.value)
+    if (!isNumber) {
+      phoneNumber.value = phoneNumber.value.slice(0, -1)
+    }
+  })
+
+
+  function handleSubmit(){
+  console.log('submit');
+  }
+</script>
+
+<style scoped>
+
+</style>

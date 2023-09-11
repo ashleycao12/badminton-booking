a72c7aff08cabecd5cfadbf353fa79d6330da870
Ashley Cao <ashleycao12@gmail.com>
2023-09-11 16:38:13 +1200

    add firestore fet function

--- composables/useFirestore.ts
+++ composables/useFirestore.ts
@@ -1,9 +1,15 @@
 import {collection, getDocs,  getFirestore } from "firebase/firestore"
 
-const db = getFirestore()
-const bookingRef = collection(db, "bookings")
-
 export async function getBookings() {
-  const result = await getDocs(bookingRef)
+  const db = getFirestore()
+  const bookingRef = collection(db, "bookings")
+  const querySnapshot  = await getDocs(bookingRef)
+  const result:booking[] = []
+  querySnapshot.forEach((doc) => {
+    result.push({
+      dateTime: new Date(doc.data().dateTime.seconds*1000),
+      userId: doc.data().userId
+    })
+  });
   return result
 }

981bb83dcf3e8c2605573be34e047d5517ed06d5
Ashley Cao <ashleycao12@gmail.com>
2023-12-05 21:55:09 +1300

    change logo and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app:{
+    head:{
+      title: 'Cầu lông Phước Thiện'
+    }
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/i18n'
